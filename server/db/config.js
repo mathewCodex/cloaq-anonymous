250909d6e24e3c21587eb808a4d641f3365a4b09
mathewCodex <96009076+mathewCodex@users.noreply.github.com>
2025-09-20 18:00:51 +0100

    Update config.js

--- server/db/config.js
+++ server/db/config.js
@@ -1,43 +1,17 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB Connected...");
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
-let cached = global._mongoCached; // reuse across lambda invocations in Vercel
-if (!cached) {
-  cached = global._mongoCached = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-  if (!cached.promise) {
-    const opts = {
-      // use New URL parser and unified topology
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    };
-    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then(m => {
-      return m;
+      useUnifiedTopology: true,
     });
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+};
 
 module.exports = connectDB;
+
